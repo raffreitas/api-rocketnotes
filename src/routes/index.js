822862c13aa868ceafbc1fcbc71d5e6014f7ebc8
Rafael Freitas <rafaarantes@live.com>
2022-08-04 20:59:50 -0300

    finished controllers

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,13 @@
 const { Router } = require("express");
 
-const userRoutes = require("./user.routes");
+const userRouter = require("./user.routes");
+const notesRouter = require("./notes.routes");
+const tagsRouter = require("./tags.routes");
 
 const routes = Router();
 
-routes.use("/users", userRoutes);
+routes.use("/users", userRouter);
+routes.use("/notes", notesRouter);
+routes.use("/tags", tagsRouter);
 
 module.exports = routes;
